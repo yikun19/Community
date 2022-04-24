c2b91753432d75106279a73ed9b670a47678e0fb
Y3ko <luoyikun19@19.com>
2022-04-24 16:53:59 +0800

    add index and bootstrap

--- src/main/java/com/example/demo/Contorller.java
+++ src/main/java/com/example/demo/Contorller.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class Contorller {
 
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam (name = "name", required = false, defaultValue = "World") String name, Model model) {
+    @GetMapping("/")
+    public String index(@RequestParam (name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "index";
     }
 }
